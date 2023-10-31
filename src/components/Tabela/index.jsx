f49d51cef852c00c87689b3f176ef446a77893ba
Luccas69 <luccas.rossarolla@gmail.com>
2023-10-31 19:24:20 -0400

    criação da tabela

--- src/components/Tabela/index.jsx
+++ src/components/Tabela/index.jsx
@@ -1,9 +1,46 @@
 import styles from './Tabela.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Tabela() {
+
+    const [salas, setSalas] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3001/salas')
+            .then((response) => {
+                setSalas(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+
+
     return (
         <div className={styles.tabela}>
-            <h1>Tabelinha</h1>
+            <h1>
+                Reservas realizadas:
+            </h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Descrição</th>
+                        <th>Solicitante</th>
+                        <th>Sala</th>
+                        <th>Início</th>
+                        <th>Fim</th>
+                    </tr>
+                    <tr>
+                        <td>Aula de Frameworks</td>
+                        <td>José da Silva</td>
+                        <td>Bloco A - Lab. de Informática</td>
+                        <td>25/10/2023 às 19:00:00</td>
+                        <td>25/10/2023 às 20:00:00</td>
+                    </tr>
+                </thead>
+            </table>
         </div>
     )
 }
