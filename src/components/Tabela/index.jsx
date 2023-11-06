619ce7c614738d3692a7a6021a8d67b6f50cc222
Luccas69 <luccas.rossarolla@gmail.com>
2023-11-06 19:23:16 -0400

    table alterada

--- src/components/Tabela/index.jsx
+++ src/components/Tabela/index.jsx
@@ -16,35 +16,49 @@ export default function Tabela() {
             })
     }, [])
 
+    function formatarData(data) {
+        const dataFormatada = new Date(data)
+        const dia = dataFormatada.getDate().toString().padStart(2, '0')
+        const mes = (dataFormatada.getMonth() + 1).toString().padStart(2, '0')
+        const ano = dataFormatada.getFullYear()
+        const hora = dataFormatada.getHours().toString().padStart(2, '0')
+        const minuto = dataFormatada.getMinutes().toString().padStart(2, '0')
+        const segundo = dataFormatada.getSeconds().toString().padStart(2, '0')
 
+        return `${dia}/${mes}/${ano} às ${hora}:${minuto}:${segundo}`
+    }
 
     return (
-        <div className={styles.tabela}>
-            <h1>
-                Reservas realizadas:
-            </h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Descrição</th>
-                        <th>Solicitante</th>
-                        <th>Sala</th>
-                        <th>Início</th>
-                        <th>Fim</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {salas.map((salas) => (
-                    <tr key={salas.id}>
-                        <td>{salas.descricao}</td>
-                        <td>{salas.solicitante}</td>
-                        <td>{salas.sala}</td>
-                        <td>{salas.inicio}</td>
-                        <td>{salas.fim}</td>
-                    </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <>
+            <div className={styles.title}>
+                <h1>
+                    Reservas realizadas: {salas.length}
+                </h1>
+            </div>
+            <div className={styles.container}>
+                <table className={styles.table}>
+                    <thead className={styles.head}>
+                        <tr>
+                            <th>Descrição</th>
+                            <th>Solicitante</th>
+                            <th>Sala</th>
+                            <th>Início</th>
+                            <th>Fim</th>
+                        </tr>
+                    </thead>
+                    <tbody className={styles.body}>
+                        {salas.map((salas) => (
+                            <tr key={salas.id}>
+                                <td>{salas.descricao}</td>
+                                <td>{salas.solicitante}</td>
+                                <td>{salas.sala}</td>
+                                <td>{formatarData(salas.inicio)}</td>
+                                <td>{formatarData(salas.fim)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
